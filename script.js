1af844b737f4b6837a8b48a134a69f12287fd712
kakelly83 <70811100+kakelly83@users.noreply.github.com>
2021-12-30 20:07:34 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -157,6 +157,7 @@
             if (progress > .265) {
               _this.$icon2.css({
                 'z-index': 10000
+                  });
             } else {
               _this.$icon2.css({
                 'z-index': 1
